24935bc4fbe2356bd1f0735f65e377760c200e94
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-23 13:53:22 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int SumOfDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number/=10;
+    }
+    return sum;
+}
+
+int number = 452;
+Console.WriteLine(SumOfDigits(number));
+
+number = 82;
+Console.WriteLine(SumOfDigits(number));
+
+number = 9012;
+Console.WriteLine(SumOfDigits(number));
